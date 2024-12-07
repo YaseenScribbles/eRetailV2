ec0ef45b218959786452a1757b568b9b4c57443e
YaseenScribbles <yaseen0696@gmail.com>
2024-12-07 16:31:35 +0530

    reduce fps

--- resources/js/Pages/components/Scanner.jsx
+++ resources/js/Pages/components/Scanner.jsx
@@ -1,36 +1,49 @@
 import React, { useEffect, useRef } from "react";
-import { Html5QrcodeScanner } from "html5-qrcode";
+import { Html5QrcodeScanner, Html5QrcodeSupportedFormats } from "html5-qrcode";
 
 const Scanner = ({ setBarcode, setErrors, setShowScanner }) => {
-
-    const scannerRef = useRef(null)
+    const scannerRef = useRef(null);
 
     useEffect(() => {
-        let scanner = new Html5QrcodeScanner(
-            "reader",
-            {
-                fps: 10,
-                qrbox: { width: 350, height: 350 },
+        let scanner = new Html5QrcodeScanner("reader", {
+            fps: 5,
+            qrbox: { width: 500, height: 150 },
+            useBarCodeDetectorIfSupported: true,
+            formatsToSupport: [
+                Html5QrcodeSupportedFormats.CODE_128,
+                Html5QrcodeSupportedFormats.QR_CODE,
+            ],
+            videoConstraints: {
+                width: { ideal: 1280 },
+                height: { ideal: 720 },
             },
-            false
-        );
+        });
 
         const success = async (result) => {
             await scanner.clear();
             setBarcode(result);
-            setShowScanner(false)
+            setShowScanner(false);
         };
 
         const error = (err) => {
-            console.warn(err);
+            if (err.message) setErrors((prev) => [...prev, err.message]);
         };
 
         scanner.render(success, error);
+
+        return () => {
+            scanner
+                .clear()
+                .catch((err) => console.error("Failed to clear scanner", err));
+        };
     }, []);
 
     useEffect(() => {
         const handleClickOutside = (event) => {
-            if (scannerRef.current && !scannerRef.current.contains(event.target)) {
+            if (
+                scannerRef.current &&
+                !scannerRef.current.contains(event.target)
+            ) {
                 setShowScanner(false); // Close scanner on outside click
             }
         };
